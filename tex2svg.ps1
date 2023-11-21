01acd241dccf914b4bdc15817a8c5bce96ccd437
sakuratsuyu <sakuratusyu@outlook.com>
2023-11-21 16:57:44 +0800

    [CHORE] Add tex to svg script.

---
+++ tex2svg.ps1
@@ -0,0 +1,22 @@
+param($PATH)
+
+$CURRENT = (Get-Location).Path
+echo $CURRENT
+
+$BASENAME = (Split-Path $PATH)
+$TEX = (Split-Path $PATH -Leaf)
+$FILENAME = (Split-Path $PATH -LeafBase)
+
+$XDV = $FILENAME + ".xdv"
+$SVG = $FILENAME + ".svg"
+$AUX = $FILENAME + ".aux"
+$LOG = $FILENAME + ".log"
+
+cd $BASENAME
+
+xelatex -no-pdf -interaction=batchmode -halt-on-error $TEX
+dvisvgm $XDV -n -v 0 -o $SVG
+Remove-Item $XDV,$AUX,$LOG
+Remove-Item *.pdf,*synctex.gz
+
+cd $CURRENT
